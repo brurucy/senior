17a6003e2299d883ff990d30b40192953491f7db
brurucy <brurucy@gmail.com>
2023-07-20 00:34:34 +0300

    polished the mvp

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,117 @@
+use inquire::Confirm;
 use reqwest::Url;
 use serde::{Deserialize, Serialize};
 use std::env;
 use std::fs::read_to_string;
-use tree_sitter::Parser;
+
 use tree_sitter_traversal::{traverse_tree, Order};
 
 const OPENAI_API_KEY_ENV_VAR: &str = "OPENAI_API_KEY";
 const COMPLETION_URL: &str = "https://api.openai.com/v1/chat/completions";
 
-struct Optimizer {
+struct Optimizer<'a> {
     file_name: String,
     function_name: String,
     source_file: String,
-    context: String,
+    function: String,
+    optional_context: String,
     model: String,
+    theme: String,
+    dry_run: bool,
+    add_comments: bool,
+    skip_prompt: bool,
+    tree: Option<tree_sitter::Tree>,
+    function_node: Option<tree_sitter::Node<'a >>,
 }
 
-impl Default for Optimizer {
+impl<'a> Default for Optimizer<'a> {
     fn default() -> Self {
         return Self {
             file_name: Default::default(),
             function_name: Default::default(),
             source_file: Default::default(),
-            context: Default::default(),
+            function: Default::default(),
+            optional_context: Default::default(),
             model: "gpt-3.5-turbo".to_string(),
+            theme: Default::default(),
+            dry_run: false,
+            add_comments: false,
+            skip_prompt: false,
+            tree: Default::default(),
+            function_node: Default::default(),
         };
     }
 }
 
-impl Optimizer {
-    fn new(file_name: &str, function_name: &str, optional_context: &str) -> Self {
-        let mut chat_gpt: Optimizer = Default::default();
+fn draft_instructions(function: &str, add_comments: bool, optional_context: &str) -> String {
+    let add_comments = if add_comments { "do" } else { "don't" };
 
-        chat_gpt.file_name = file_name.to_string();
-        chat_gpt.function_name = function_name.to_string();
-        chat_gpt.source_file = read_to_string(&chat_gpt.file_name).unwrap();
-        chat_gpt.context = optional_context.to_string();
+    return format!(
+        r#"You are a senior software engineer.
 
-        let mut parser = Parser::new();
+Your task is to optimise the following function: {}
+
+Strictly adhere to the following instructions:
+1. Optimise it without changing the type signature, and only propose small, incremental changes
+2. {} add comments.
+3. Respond only with code, and nothing else
+3. If the code cannot be optimised further, respond with "OPTIMAL"
+4. {}
+"#,
+        function, add_comments, optional_context
+    );
+}
+
+impl<'a> Optimizer<'a> {
+    fn new(
+        file_name: &str,
+        function_name: &str,
+        optional_context: &str,
+        theme: &str,
+        model: &str,
+        dry_run: bool,
+        add_comments: bool,
+        skip_prompt: bool,
+    ) -> Self {
+        let mut optimizer: Optimizer = Default::default();
+
+        optimizer.file_name = file_name.to_string();
+        optimizer.function_name = function_name.to_string();
+        optimizer.source_file = read_to_string(&optimizer.file_name).unwrap();
+        optimizer.optional_context = optional_context.to_string();
+        optimizer.model = model.to_string();
+        optimizer.theme = theme.to_string();
+        optimizer.dry_run = dry_run;
+        optimizer.add_comments = add_comments;
+        optimizer.skip_prompt = skip_prompt;
+        optimizer.tree = None;
+
+        return optimizer;
+    }
+    fn build(&mut self) -> Result<(), String> {
+        let mut parser = tree_sitter::Parser::new();
         parser.set_language(tree_sitter_rust::language()).unwrap();
-        let tree = parser.parse(&chat_gpt.context, None).unwrap();
-        let source = chat_gpt.context.clone().into_bytes();
-        let function_node = traverse_tree(&tree, Order::Pre)
-            .find(|node| {
+        if let Some(tree) = parser.parse(&self.source_file, None) {
+            let source_bytes = self.source_file.as_bytes();
+            if let Some(node) = traverse_tree(&tree, Order::Pre).find(|node| {
                 let node_kind = node.kind();
                 if node_kind == "function_item" {
                     let node_function_name = node.child_by_field_name("name").unwrap();
-                    return node_function_name.utf8_text(&source).unwrap() == function_name;
+                    return node_function_name.utf8_text(&source_bytes).unwrap()
+                        == self.function_name;
                 }
 
                 return false;
-            })
-            .unwrap()
-            .utf8_text(&source)
-            .unwrap();
-        chat_gpt.context = function_node.to_string();
+            }) {
+                self.function = node.utf8_text(source_bytes).unwrap().to_string();
+            } else {
+                return Err(format!("failed to find function: {}", self.function_name));
+            }
+        } else {
+            return Err(String::from("failed to parse source file"));
+        }
 
-        return chat_gpt;
-    }
-    fn draft_instructions(&self) -> String {
-        let instructions = format!(
-            r#"You are a program optimizer.
-
-This is the starting point:
-{}
-
-Your task is to optimise the function named: {}
-
-Here is some context: {}
-
-Optimise it without changing the type signature, and only propose small, incremental changes.
-
-Respond only with the optimised code, without any comments whatsover, at all.
-
-If the code cannot be optimised further, respond with "OPTIMAL".
-"#,
-            self.source_file, self.function_name, self.context
-        );
-
-        return instructions;
+        return Ok(());
     }
 }
 
@@ -96,7 +129,6 @@ struct OpenAIChatRequest {
 
 #[derive(Deserialize, Debug)]
 struct OpenAIChatResponseChoice {
-    index: usize,
     message: Message,
 }
 
@@ -105,8 +137,8 @@ struct OpenAIChatResponse {
     choices: Vec<OpenAIChatResponseChoice>,
 }
 
-impl Optimizer {
-    async fn optimise(&mut self, secret: &str) -> Option<String> {
+impl<'a> Optimizer<'a> {
+    async fn optimise(&mut self, secret: &str) -> Result<String, String> {
         let client = reqwest::Client::new();
         let model = self.model.clone();
 
@@ -114,47 +146,109 @@ impl Optimizer {
             model,
             messages: vec![Message {
                 role: "system".to_string(),
-                content: self.draft_instructions(),
+                content: draft_instructions(
+                    &self.function,
+                    self.add_comments,
+                    &self.optional_context,
+                ),
             }],
         };
 
         let url = Url::parse(COMPLETION_URL).unwrap();
 
-        let req = client
-            .post(url)
-            .bearer_auth(secret)
-            .json(&body)
-            .build()
-            .unwrap();
+        return match client.post(url).bearer_auth(secret).json(&body).build() {
+            Ok(req) => match client.execute(req).await {
+                Ok(resp) => match resp.json::<OpenAIChatResponse>().await {
+                    Ok(parsed_resp) => {
+                        let content = parsed_resp.choices[0].message.content.clone();
+                        if content == "OPTIMAL" {
+                            return Err("optimal".to_string());
+                        }
 
-        let resp = client
-            .execute(req)
-            .await
-            .unwrap()
-            .json::<OpenAIChatResponse>()
-            .await
-            .unwrap();
-
-        let content = resp.choices[0].message.content.clone();
-
-        if content == "OPTIMAL" {
-            return None;
-        }
-
-        return Some(content);
+                        return Ok(content);
+                    }
+                    Err(e) => Err(e.to_string()),
+                },
+                Err(e) => Err(e.to_string()),
+            },
+            Err(e) => Err(e.to_string()),
+        };
     }
+    fn edit_source_file(&mut self, suggestion: &[u8]) {
+        let parsed_suggestion = tree_sitter::Parser:
+    }
+}
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Location of the source code file
+    #[arg(short, long)]
+    file_location: String,
+
+    /// Name of the function that will be searched for in the file at the given file_location
+    #[arg(short, long)]
+    function_name: String,
+
+    /// The `bat` theme. Check out https://github.com/sharkdp/bat/tree/master/assets/themes for a list of available themes
+    #[arg(short, long)]
+    theme: String,
+
+    /// The OpenAI model. Check out https://platform.openai.com/docs/models/overview
+    #[arg(short, long)]
+    model: String,
+
+    /// Anything else you would like to tell your chosen LLM
+    #[arg(short, long)]
+    optional_context: String,
+
+    /// Setting this option to true will print out the suggestion without a confirmation prompt
+    #[arg(short, long)]
+    dry_run: bool,
+
+    /// Setting this option to true will print and automatically overwrite the function in the source file
+    #[arg(short, long)]
+    skip_prompt: bool,
+
+    /// Should the new code have comments?
+    #[arg(short, long)]
+    add_comments: bool,
 }
 
 #[tokio::main]
 async fn main() {
+    let args = Args::parse();
     let secret = env::var(OPENAI_API_KEY_ENV_VAR).unwrap();
 
-    let mut opt = Optimizer::new("../indexset/src/lib.rs", "insert", "");
+    let mut opt = Optimizer::new(
+        &args.file_location,
+        &args.function_name,
+        &args.optional_context,
+        &args.theme,
+        &args.model,
+        args.dry_run,
+        args.add_comments,
+        args.skip_prompt,
+    );
     let output = opt.optimise(&secret).await.unwrap();
 
     bat::PrettyPrinter::new()
         .input_from_bytes(output.as_bytes())
         .language("rust")
+        .vcs_modification_markers(true)
         .print()
         .unwrap();
+
+    if !opt.skip_prompt {
+        Confirm::new("Apply the suggestion?")
+            .with_default(false)
+            .prompt()
+            .unwrap();
+    }
+
+    if !opt.dry_run {
+        opt.edit_source_file(output.as_bytes())
+    }
 }
